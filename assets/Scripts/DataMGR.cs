38af753b20e15db1c3e74d571b9c129cf81caace
vedprakashgupta <vedprakashgupta@outlook.com>
2018-03-27 09:10:46 +0530

    Project Commite
    Committing the Assets folder and Project settings. Unity Version 2017.3.0f3

---
+++ assets/Scripts/DataMGR.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+using System;
+public class DataMGR : MonoBehaviour 
+{
+	public delegate void DataGetFromServer(string question, string publishTime, string[] choiceList, int[] voteList);
+	public static event DataGetFromServer DataBack;
+
+	public delegate void RefreshButtonStatus();
+	public static event RefreshButtonStatus RBStatus;
+	//public void ProcessData(string question, string publishTime, string[] choiceList, int[] voteList)
+
+	 public string URL="https://private-5b1d8-sampleapi187.apiary-mock.com/questions";
+	[HideInInspector]
+	public string _Question;
+	[HideInInspector]
+	public string _PubishedAt;
+	[HideInInspector]
+	public List<string>  _ChoiceList_PL;
+	[HideInInspector]
+	public List<int>  _VoteList_PV; 
+
+	void OnEnable()
+	{
+		GameMGR.Click +=StartLoad;
+	}
+
+	public void StartLoad()
+	{
+		StartCoroutine ("LoadStart");
+	}
+
+	IEnumerator LoadStart()
+	{
+		WWW www = new WWW(URL);
+		yield return www;
+		if (www.error == null)
+		{
+			SaveAndProcessData(www.text);
+
+		}
+		else
+		{
+			Debug.Log("ERROR: " + www.error);
+			RBStatus ();
+		}        
+	}    
+
+	void SaveAndProcessData(string str)
+	{
+		str = str.Substring (1,str.Length-2);
+		var N = JSON.Parse(str);
+		_Question = N["question"].Value;      
+		_PubishedAt = N["published_at"].Value; 
+		var l = N ["choices"].Count;
+		_ChoiceList_PL.Clear ();
+		_VoteList_PV.Clear ();
+		for(int i=0; i<l; i++)
+		{
+			_ChoiceList_PL.Add(N ["choices"] [i] ["choice"].Value);
+			_VoteList_PV.Add( N ["choices"] [i] ["votes"].AsInt);
+		}
+
+		DataBack (_Question, _PubishedAt, _ChoiceList_PL.ToArray (), _VoteList_PV.ToArray ());
+		RBStatus ();
+	}
+
+	void OnDisable()
+	{
+		GameMGR.Click -=StartLoad;
+	}
+}
---
+++ assets/Scripts/GameMGR.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+using UnityEngine.UI;
+using System;
+public class GameMGR : MonoBehaviour 
+{
+	
+	public Text _questionText;
+	public Text[] _choicesList;
+	public Text[] _voteList;
+	public Button _refreshBTN;
+
+	public delegate void RefreshClicked();
+	public static event RefreshClicked Click;
+
+	void OnEnable()
+	{
+		DataMGR.DataBack += ProcessData;
+		DataMGR.RBStatus += RefreshBTNStatus;
+	}
+
+	//After BTN Clicked 
+	public void LoadData()
+	{
+		Click ();
+		_refreshBTN.interactable = false;
+	}
+
+	//After GettingData
+	public void ProcessData(string question, string publishTime, string[] choiceList, int[] voteList)
+	{
+		_questionText.text = question;
+		for(int i=0; i<choiceList.Length; i++)
+		{
+			_choicesList [i].text = choiceList [i];
+			_voteList [i].text = voteList [i].ToString ();
+			_voteList [i].transform.parent.gameObject.SetActive (true);
+		}
+	}
+
+	//Status Of BTN
+	void RefreshBTNStatus()
+	{
+		_refreshBTN.interactable = true;
+	}
+
+	void OnDisable()
+	{
+		DataMGR.DataBack -= ProcessData;
+		DataMGR.RBStatus -= RefreshBTNStatus;
+	}
+
+}
